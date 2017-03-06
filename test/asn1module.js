402187d570d6634db02aea11dc228835df2c28c6
mt <mt>
2017-03-06 23:07:52 +0100

    set up mocha and chai

---
+++ test/asn1module.js
@@ -0,0 +1,10 @@
+var expect    = require("chai").expect;
+var asn1encoder = require("../app/asn1/asn1module.js");
+
+describe("ASN1 Module", function() {
+  describe("Encoder", function() {
+	  it("encodes primitive types", function() {
+
+    });
+  });
+});
